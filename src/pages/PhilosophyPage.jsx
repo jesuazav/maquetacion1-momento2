39d21e0860c6f264a0d193c81698853ebd33ae12
Jefferson suaza <jsuaza1@cesde.net>
2026-04-21 09:37:29 -0500

    creacion de pagina philosophy con css

---
+++ src/pages/PhilosophyPage.jsx
@@ -0,0 +1,25 @@
+import PhilosophyHeader from '../components/PhilosophyHeader'
+import './PhilosophyPage.css'
+
+function PhilosophyPage() {
+  return (
+    <div className="philosophy-page">
+      <PhilosophyHeader />
+      
+      
+        <div className="philosophy-intro-container">
+          <div className="intro-text">
+            <p>
+              Nuestra filosofía se centra en la observación y el respeto por los ritmos naturales. Creemos que cultivar un Bonsái no es solo jardinería, sino una meditación activa.
+            </p>
+            <p>
+              En nuestro estudio, te enseñamos a escuchar al árbol, entendiendo que cada corte y cada alambrado debe tener un propósito. Buscamos el equilibrio perfecto entre la intervención humana y la expresión salvaje de la naturaleza.
+            </p>
+          </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default PhilosophyPage
